4aff6f0f57fbaacd81d0c82b97d543e1ba143585
stefan <stefan.kluenker@gmail.com>
2023-03-21 10:01:44 +0100

    deployment for phil"

--- screens/home.tsx
+++ screens/home.tsx
@@ -23,12 +23,12 @@ export default function HomeScreen({ navigation }) {
     async function loadFile() {
         if (Platform.OS !== 'web') {
             try {
-                let status = await Storage.getItem({ key: "opacityStatus"});
+                let status = await Storage.getItem({ key: "opacityStatus" });
                 if (status != null) {
                     setOpacityStatus(status);
                 }
             }
-            catch(e){
+            catch (e) {
                 console.log(e);
             }
         }
@@ -41,14 +41,15 @@ export default function HomeScreen({ navigation }) {
             </View>
 
             <View style={globalStyles.containerTop}>
-                <Image style={styles.logo} source={require('./../assets/icon.png')} />
+                <Image style={{ width: 200, height: 200, resizeMode: 'contain', opacity: opacityStatus/100 }} source={require('./../assets/icon.png')} />
+                <Image style={{ width: 200, height: 200, resizeMode: 'contain',  position: 'absolute',}} source={require('./../assets/icon_colorless.png')} />
             </View>
-  
+
             <View style={globalStyles.containerBottom}>
                 <TouchableOpacity style={globalStyles.button} onPress={() => navigation.navigate(technique[Math.floor(Math.random() * 2)])}>
                     <Text style={globalStyles.buttonText}>Wana</Text>
                 </TouchableOpacity>
-            </View> 
+            </View>
         </View>
     );
 }
